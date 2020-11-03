0f7ece04377585743f6fae6df8ed505062bec039
leo <leo.bravo.rain@gmail.com>
2020-11-03 12:30:59 -0300

    refactoring the VAE trainnig and model

--- work/thesis/models/classifier.py
+++ work/thesis/models/classifier.py
@@ -1,48 +1,7 @@
 from torch import nn
 import torch
 
-# implementacion adaptada a 1D de https://github.com/naoto0804/pytorch-inpainting-with-partial-conv
-
-class PartialConv(nn.Module):
-    def __init__(self, in_channels_C,in_channels_M, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True):
-        super().__init__()
-        self.input_conv = nn.Conv1d(in_channels_C, out_channels, kernel_size,
-                                    stride, padding, dilation, groups, bias)
-        self.mask_conv = nn.Conv1d(in_channels_M, out_channels, kernel_size,
-                                   stride, padding, dilation, groups, False)
-        # self.input_conv.apply(weights_init('kaiming'))
-
-        torch.nn.init.constant_(self.mask_conv.weight, 1.0)
-
-        # mask is not updated
-        for param in self.mask_conv.parameters():
-            param.requires_grad = False
-
-    def forward(self,input, mask):
-        # http://masc.cs.gmu.edu/wiki/partialconv
-        # C(X) = W^T * X + b, C(0) = b, D(M) = 1 * M + 0 = sum(M)
-        # W^T* (M .* X) / sum(M) + b = [C(M .* X) – C(0)] / D(M) + C(0)
-        #print(input.shape, mask.shape)
-        output = self.input_conv(input * mask)
-        if self.input_conv.bias is not None:
-            output_bias = self.input_conv.bias.view(1, -1, 1).expand_as(output)
-        else:
-            output_bias = torch.zeros_like(output)
-
-        with torch.no_grad():
-            output_mask = self.mask_conv(mask)
-
-        no_update_holes = output_mask == 0
-        mask_sum = output_mask.masked_fill_(no_update_holes, 1.0)
-
-        output_pre = (output - output_bias) / mask_sum + output_bias
-        output = output_pre.masked_fill_(no_update_holes, 0.0)
-
-        new_mask = torch.ones_like(output)
-        new_mask = new_mask.masked_fill_(no_update_holes, 0.0)
-
-        return output, new_mask
+from partialConvolution import PartialConv
         
         
 
