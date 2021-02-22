0ef3391898597306c5e62567514ed469a408084f
leo <leo.bravo.rain@gmail.com>
2021-02-22 12:09:11 -0300

    add code to get dataset detaisl

---
+++ work/thesis/notebooks/get_dataset_details.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Note
+# This notebook is to train the encoder as a classifier with the idea of validate the encoder architecture first and then use this to train the VAE.
+
+# # Parameters to experiment
+
+# In[1]:
+
+
+# training on guanaco
+# ATENTION: if it is going to run on guanaco:
+# 1) comment the %matplotlib magic in next block and any magic (something like %code)
+# 2) Change to True the trainingOnGuanaco vairbale
+# 3) set epoch with an appropiate number
+# 4) add comment to experiemnts
+# 5) Add this file as python file 
+# 6) Change launchJobOnGuanaco file to run this file but with python format
+trainingOnGuanaco = False
+
+# train without notebook
+trainWithJustPython = False
+
+# number_experiment (this is just a name)
+# priors:
+# 1
+number_experiment = 17
+number_experiment = str(number_experiment)
+
+# seed to generate same datasets
+seed = 0
+
+# training
+epochs = 100000
+
+# max elements by class
+max_elements_per_class = 15000
+
+# train with previous model
+trainWithPreviousModel = False
+
+# include delta errors
+includeDeltaErrors = True
+
+# band
+# passband = [5]
+passband = [0, 1, 2, 3, 4, 5]
+
+
+# include ohter feautures
+includeOtherFeatures = True
+
+# num of features to add
+# ṕvar by channel
+otherFeaturesDim = 12
+
+
+# In[2]:
+
+
+# cuda device
+cuda_device = 0
+cuda_device = "cuda:" + str(cuda_device)
+
+# classes to analyze
+# 42,  90,  16,  67,  62, 993,  92,  52,  88,  65, 991, 992,  15,
+#        95,   6,  53, 994,  64
+
+# periodic
+# only_these_labels = [16, 92, 53]
+
+# periodic + variable
+only_these_labels = [16, 92, 53, 88, 65, 6]
+# 53 has 24 light curves
+
+# only_these_labels = [16, 92]
+# only_these_labels = [16, 92]
+# only_these_labels = [42,  90,  16,  67,  62, 993,  92,  52,  88,  65, 991, 992,  15,
+#         95,   6,  53, 994,  64]
+
+# VAE parameters
+latentDim = 100
+hiddenDim = 100
+inputDim = 72
+
+batch_training_size = 128
+
+# early stopping 
+threshold_early_stop = 3000
+
+
+# In[3]:
+
+
+# training params
+learning_rate = 1e-3
+
+
+# In[4]:
+
+
+# add general comment about experiment 
+# comment = "encoder as clasifier with periodic + variable (with class balancing) + 1 conv layer more"
+comment = "exp " + number_experiment + " + encoder as clasifier with periodic + variable + class balancing + 1 conv layer more + " + str(len(passband)) + " channels + seed " + str(seed) + " + " + ("include delta errors" if includeDeltaErrors else "without delta errors") + " + max by class " + str(max_elements_per_class) + " + " + ("" if includeOtherFeatures else "not") + " other features"
+
+print(comment)
+
+
+# # Import libraries
+
+# In[5]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+from torch.utils import data
+
+# from tqdm import tqdm_notebook
+
+if not trainingOnGuanaco:
+    
+    get_ipython().run_line_magic('matplotlib', 'notebook')
+    get_ipython().run_line_magic('load_ext', 'autoreload')
+    get_ipython().run_line_magic('autoreload', '2')
+else:
+    print("not load magics")
+    
+# import functions to load dataset
+import sys
+sys.path.append("./codesToDatasets")
+from plasticc_dataset_torch import get_plasticc_datasets
+# from plasticc_plotting import plot_light_curve
+
+import math
+
+from torch import nn
+
+# local imports
+# %load_ext autoreload
+# %autoreload 2
+sys.path.append('../models')
+# from classifier import EncoderClassifier, 
+from classifierPrototype import EncoderClassifier
+
+sys.path.append("./aux/")
+from auxFunctions import *
+
+from sklearn.model_selection import train_test_split
+
+
+# ## Load the path to save model while training
+
+# In[ ]:
+
+
+import os
+
+# create experiment's folder
+tmpGuanaco = "/home/lbravo/thesis/thesis/work/thesis/"
+tmpLocal = "/home/leo/Desktop/thesis/work/thesis/"
+
+expPath = "experiments/" + number_experiment + "/seed" + str(seed) + "/maxClass" + str(int(max_elements_per_class/1000)) + "k"
+
+folder_path = (tmpGuanaco + expPath) if trainingOnGuanaco else (tmpLocal + expPath)
+# !mkdir folder_path
+# os.makedirs(os.path.dirname(folder_path), exist_ok=True)
+
+# check if folder exists
+if not(os.path.isdir(folder_path)):
+        
+    # create folder
+    try:
+        os.makedirs(folder_path)
+        
+    except OSError as error:
+        print ("Creation of the directory %s failed" % folder_path)
+        print(error)
+    else:
+        print ("Successfully created the directory %s " % folder_path)
+else:
+    print("folder already exists")
+
+# define paht to save model while training
+pathToSaveModel = (tmpGuanaco + expPath + "/model") if trainingOnGuanaco else (tmpLocal + expPath + "/model")
+
+
+# # Load data
+
+# In[7]:
+
+
+# define path to dataset
+pathToFile = "/home/shared/astro/PLAsTiCC/" if trainingOnGuanaco else "/home/leo/Downloads/plasticData/"
+
+
+# ## Loading dataset with pytorch tool
+
+# In[13]:
+
+
+# torch_dataset_lazy = get_plasticc_datasets(pathToFile)
+
+# Light curves are tensors are now [bands, [mjd, flux, err, mask],
+# lc_data, lc_label, lc_plasticc_id                              
+torch_dataset_lazy = get_plasticc_datasets(
+    pathToFile, 
+#     only_these_labels = only_these_labels, 
+    max_elements_per_class = max_elements_per_class
+)
+
+
+# In[14]:
+
+
+assert torch_dataset_lazy.__len__() != 494096, "dataset should be smaller"
+print("dataset test ok")
+
+
+# In[18]:
+
+
+torch_dataset_lazy.__len__()
+
+
+# # Spliting data (train/test)
+
+# In[19]:
+
+
+# splitting the data
+
+# get light curves ids, targets
+ids, targets, lightCurvesIds = getLightCurvesIds(torch_dataset_lazy)
+
+
+# ## Create a dataloader
+
+# In[38]:
+
+
+# print("initila distribution")
+# initialClassesDistribution = countClasses(trainDataset, only_these_labels)
+values, counting = np.unique(targets, return_counts=True)
+
+# print("classes\n")
+# print(values)
+
+# print("counting\n")
+# print(counting)
+
+for i in np.arange(len(values)):
+
+    print(f"class: {values[i]}: \n {counting[i]}\n")
+    
+# fig, ax = plt.subplots()
+# ax.bar(x = np.arange(len(only_these_labels)), height = initialClassesDistribution)
+
+
+# In[31]:
+
+
+print(np.sum(counting))
+
